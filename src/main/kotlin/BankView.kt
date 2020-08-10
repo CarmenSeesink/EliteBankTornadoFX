8af5f63776880de7bcf4d5696af6bed1d817d551
Carmen <carmens@dotcomsoftware.co.za>
2020-08-10 10:23:39 +0200

    Add expandable table

--- src/main/kotlin/BankView.kt
+++ src/main/kotlin/BankView.kt
@@ -1,40 +1,44 @@
+import javafx.collections.ObservableList
 import javafx.geometry.Pos
 import javafx.scene.paint.Color
 import tornadofx.*
 import java.time.LocalDate
 import java.time.Period
 
-class BankView: View("BlackJack") {
+class BankView: View("EliteBank") {
     var showText = true
 
-    private val persons = listOf(
-        Person(1,"Samantha Stuart",LocalDate.of(1981,12,4)),
-        Person(2,"Tom Marks",LocalDate.of(2001,1,23)),
-        Person(3,"Stuart Gills",LocalDate.of(1989,5,23)),
-        Person(3,"Nicole Williams",LocalDate.of(1998,8,11))
-    ).asObservable()
-
-    override val root = vbox {
-        setPrefSize(1440.0, 960.0)
-        label("This is the BlackJack Game"){
-            hboxConstraints {
-                alignment = Pos.CENTER
+    override val root = tableview(clients) {
+        readonlyColumn("ID",Client::id)
+        readonlyColumn("Name", Client::name)
+        readonlyColumn("Country", Client::country)
+        rowExpander(expandOnDoubleClick = true) {
+            paddingLeft = expanderColumn.width
+            tableview(it.branches) {
+                readonlyColumn("ID",Account::id)
+                readonlyColumn("Type",Account::facilityCode)
+                readonlyColumn("Number",Account::city)
+                readonlyColumn("Balance",Account::stateProvince)
             }
-            textFill = c("#555555")
-            style {
-                backgroundColor += Color.AQUA
-            }
-            isVisible = showText
-        }
-        tableview(persons) {
-            readonlyColumn("ID",Person::id)
-            readonlyColumn("Name", Person::name)
-            readonlyColumn("Birthday", Person::birthday)
-            readonlyColumn("Age",Person::age)
         }
     }
 }
 
-class Person(val id: Int, val name: String, val birthday: LocalDate) {
-    val age: Int get() = Period.between(birthday, LocalDate.now()).years
-}
+class Client(val id: Int, val name: String, val country: String, val branches: ObservableList<Account>)
+
+class Account(val id: Int, val facilityCode: String, val city: String, val stateProvince: String)
+
+val clients = listOf(
+    Client(1,"Pacific Northwest", "USA",listOf(
+        Account(1,"D","Seattle","WA"),
+        Account(2,"W","Portland","OR")
+    ).asObservable()),
+    Client(2,"Alberta", "Canada",listOf(
+        Account(3,"W","Calgary","AB")
+    ).asObservable()),
+    Client(3,"Midwest", "USA", listOf(
+        Account(4,"D","Chicago","IL"),
+        Account(5,"D","Frankfort","KY"),
+        Account(6, "W","Indianapolis", "IN")
+    ).asObservable())
+).asObservable()
